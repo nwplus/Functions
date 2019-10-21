25d9e07099107d23598d4c4978b8f9a03a89f808
Giulio Rossi <ciuffi9@mac.com>
2019-10-20 17:44:00 -0700

    fix mailing list function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,55 +4,70 @@ import * as corsModule from "cors";
 const cors = corsModule({origin: true})
 //import * as Parser from 'json2csv'
 admin.initializeApp()
-// const Mailchimp = require('mailchimp-api-v3');
-// const API_KEY: string = functions.config().mailchimp.key
+const Mailchimp = require('mailchimp-api-v3');
+const API_KEY: string = functions.config().mailchimp.key
 const adminPass = functions.config().cms.pass
-// const mailchimp = new Mailchimp(API_KEY)
-// // // Start writing Firebase Functions
-// // // https://firebase.google.com/docs/functions/typescript
-// //
-// export const subscribeToMailingList = functions.https.onRequest(async (request, response) => {
-//     return cors(request, response, async () => {
-//         if (request.body.email_address == '') {
-//             response.sendStatus(400)
-//             return
-//         }
-//     try {
-//         const reply = await mailchimp.post('lists/d405dfd8ce/', {
-//             members: [
-//                 {
-//                     "email_address": request.body.email_address,
-//                     "status": 'subscribed'
-//                 },
-//             ]})
-//         if (reply.error_count > 0){
-//             console.log("Mailchimp errors")
-//             const error: String = reply.errors[0].error
-//             if (error.indexOf('already a list member') !== -1) {
-//                 response.status(502).send({errors: 'This email has already signed up'})
-//                 return
-//             }else if ((error.indexOf('looks fake or invalid') !== -1)||
-//                        (error.indexOf('valid email address') !== -1)) {
-//                 response.status(502).send({errors: error})
-//                 return
-//             }else {
-//                 console.log(reply.errors)
-//                 response.status(502).send({errors: 'Unexpected Mailchimp error'})
-//                 return
-//             }
-//         }else {
-//             response.sendStatus(200)
-//             return
-//         }
+const mailchimp = new Mailchimp(API_KEY)
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const subscribeToMailingList = functions.https.onRequest(async (request, response) => {
+    return cors(request, response, async () => {
+        if (request.body.email_address === '') {
+            response.sendStatus(400)
+            return
+        }
+    const mailigListId = '711520562b'
+    console.log(`Using mailing list id: ${mailigListId}`)
+    try {
+        const reply = await mailchimp.post(`lists/${mailigListId}/`, {
+            members: [
+                {
+                    "email_address": request.body.email_address,
+                    "status": 'subscribed'
+                },
+            ]})
+        if (reply.error_count > 0){
+            console.log("Mailchimp errors")
+            const error: String = reply.errors[0].error
+            if (error.indexOf('already a list member') !== -1) {
+                response.status(502).send({errors: 'This email has already signed up'})
+                return
+            }else if ((error.indexOf('looks fake or invalid') !== -1)||
+                       (error.indexOf('valid email address') !== -1)) {
+                response.status(502).send({errors: error})
+                return
+            }else {
+                console.log(reply.errors)
+                response.status(502).send({errors: 'Unexpected Mailchimp error'})
+                return
+            }
+        }else {
+            response.sendStatus(200)
+            return
+        }
 
-//     }catch (e){
-//         console.log("Server error")
-//         console.log(e)
-//         response.sendStatus(500)
-//         return
-//     }
-//     })
-// });
+    }catch (e){
+        console.log("Server error")
+        console.log(e)
+        response.sendStatus(500)
+        return
+    }
+    })
+});
+
+
+export const getAdminTokens = functions.https.onRequest( async (request, response) => {
+    const db = admin.firestore()
+    const admins = await db.collection('admins').get()
+    const adminIDs = admins.docs.map((doc) => doc.id)
+    await Promise.all(adminIDs.map(async (id) => {
+        const user = await admin.auth().getUser(id)
+        console.log(`Claims for ${user.email}`)
+        console.log(user.customClaims)
+    }))
+})
+
 const updateAdminIDs = async () => {
     const db = admin.firestore()
     const admins = await db.collection('admins').get()
